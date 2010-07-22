69786b2a572cdea93e813b973080b894c5f983cd
timonbijlsma <timonbijlsma@851c2ae9-d669-a203-e845-8c8e74a92a4f>
2010-07-22 14:39:48 +0000

    Implemented lineseg <-> lineseg collisions.

--- DanmakuTime/DanmakuTime/src/nl/weeaboo/dt/collision/ColUtil.java
+++ DanmakuTime/DanmakuTime/src/nl/weeaboo/dt/collision/ColUtil.java
@@ -1,8 +1,7 @@
 package nl.weeaboo.dt.collision;
 
-import java.awt.geom.Point2D;
+import java.awt.geom.Line2D;
 
-import nl.weeaboo.dt.DTLog;
 import nl.weeaboo.dt.object.IDrawable;
 
 public final class ColUtil {
@@ -16,50 +15,30 @@ public final class ColUtil {
 	}
 
 	public static boolean intersectCircleLineSeg(CircleColNode c0, LineSegColNode c1) {
-		double c0x = c0.getCenterX();
-		double c0y = c0.getCenterY();
-
-		Point2D p = closestPointOnLineSegment(c1, c0x, c0y);
-		
-		double dx = c0x - p.getX();
-		double dy = c0y - p.getY();
+		double distSq = Line2D.ptSegDistSq(c1.getX0(), c1.getY0(), c1.getX1(), c1.getY1(),
+				c0.getCenterX(), c0.getCenterY());
 		double r = c0.getBoundingRectangleRadius() + c1.getThickness();
-
-		//System.out.printf("(%.2f,%.2f) (dx=%.2f,dy=%.2f) %.2f\n",
-				//c1.getCenterX(), c1.getCenterY(), dx, dy, r);
-		
-		return (dx*dx) + (dy*dy) <= (r*r);
+		return distSq <= (r*r);
 	}
-
+	
 	public static boolean intersectLineSegLineSeg(LineSegColNode c0, LineSegColNode c1) {
-		DTLog.message("lineseg <-> lineseg not implemented yet");
-		return false;
+		double r = c0.getThickness() + c1.getThickness();
+		double rsq = r * r;
+		
+		Line2D.Double l0 = new Line2D.Double(c0.getX0(), c0.getY0(), c0.getX1(), c0.getY1());
+		Line2D.Double l1 = new Line2D.Double(c1.getX0(), c1.getY0(), c1.getX1(), c1.getY1());
+		
+		return l0.ptLineDistSq(l1.x1, l1.y1) <= rsq
+			|| l0.ptLineDistSq(l1.x2, l1.y2) <= rsq
+			|| l1.ptLineDistSq(l0.x1, l0.y1) <= rsq
+			|| l1.ptLineDistSq(l0.x2, l0.y2) <= rsq
+			|| l0.intersectsLine(l1);
 	}
 	
 	public static boolean intersectUndefined(IColNode c0, IColNode c1) {
 		throw new IllegalArgumentException(String.format(
 				"Undefined combination of collision node types: %s <-> %s",
 				c0.getClass().getSimpleName(), c1.getClass().getSimpleName()));
-	}
-
-	public static Point2D closestPointOnLineSegment(LineSegColNode node, double x, double y) {
-		double x0 = node.getX0();
-		double y0 = node.getY0();
-		double dx = x - x0;
-		double dy = y - y0;
-
-		double ux = node.getUnitLengthX();
-		double uy = node.getUnitLengthY();
-
-		double t = Math.abs(ux*dx) + Math.abs(uy*dy);
-		
-		if (t <= 0) {
-			return new Point2D.Double(x0, y0);
-		} else if (t >= node.getLength()) {
-			return new Point2D.Double(node.getX1(), node.getY1());
-		} else {
-			return new Point2D.Double(x0 + ux * t, y0 + uy * t);
-		}
 	}
 	
 	public static void collide(IColNode a, IColNode b, boolean atob, boolean btoa) {
